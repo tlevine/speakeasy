4276f6729d177f75b378bfb7e503afcfd65555cc
Thomas Levine <_@thomaslevine.com>
2014-08-23 02:06:01 +0000

    -

--- music.py
+++ music.py
@@ -5,23 +5,63 @@ import os
 import pyfiglet
 import colorama as c
 
-texts = [
+ITEMS = [
+    'Boneless Pork Tenderloin',
+    'Boneless Top Round Roast',
+    'Split Chicken Breast',
+    'Tilapia Fillets',
+    'Krakus Polish Ham',
+    'Chicken Salad With Fruit',
+    'Personal Size Watermelon',
+    'Golden Ripe Pineapples',
+    'Juicy Cantalopes',
+    'Kraft Salad Dressing',
+    'Langers Cranberry 100% Juice', # Plus
+    'Pasta Sale Elbows Farfalle',
+    'Tri Color Primavera',
+    'Viva Paper Towels',
+    'OREO Cookies',
+    'Split Top White Bread',
+    'Minute Maid Drinks',
+    'Lindy\'s Italian Ice',
+    'Blueberry Pie',
+    '4% Off Your Purchase',
 ]
 
 COLORS = [
-    c.Fore.WHITE,
     c.Fore.CYAN,
     c.Fore.YELLOW,
     c.Fore.MAGENTA,
+    c.Fore.WHITE,
 ]
+
+def item():
+    item_string = random.choice(ITEMS)
+    split = item_string.split()
+    if len(split) == 2:
+        word = [split[0], split[0], split[1], split[1]]
+        colors = [COLORS[0], COLORS[0], COLORS[1], COLORS[1]]
+    elif len(split) == 3:
+        word = split + [split[2]]
+        colors = COLORS + [COLORS[2]]
+    elif len(split) == 4:
+        word = split
+        colors = COLORS
+    else:
+        return None
+    return zip(colors, word)
+
 f = pyfiglet.Figlet()
 
 try:
     while True:
-        for color in COLORS:
+        words = item()
+        if words == None:
+            continue
+        f.setFont(font = random.choice(f.getFonts()))
+        for color, word in words:
             _ = input()
             os.system("clear") 
-            f.setFont(font = random.choice(f.getFonts()))
-            print(color + f.renderText(random.choice(texts)))
+            print(color + f.renderText(word))
 except KeyboardInterrupt:
     pass
